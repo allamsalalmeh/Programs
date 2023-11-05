45bfa669b19583b180c997863814d1d692b4ec49
allam <allamsalameh@gmail.com>
2023-11-05 10:21:47 +0300

    1- use while instead of goto. 2- loop between 2 and  sqrt(n) instead of looping between 1 and n. 3- display the user input incase not valid.

--- GCF/Program.cs
+++ GCF/Program.cs
@@ -1,35 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("This Program to Calculate GCF");
-Console.WriteLine("Enter number bigger than 1 to start or 0 to exit");
-int i;
-allam:
-if (int.TryParse(Console.ReadLine(), out i)) {
-    while (i != 0)
+int n;
+
+while (true)
+{
+    Console.WriteLine("Enter number bigger than 1 to start or 0 to exit");
+    var str = Console.ReadLine();
+    if (!int.TryParse(str, out n))
     {
-        int j = 1;
-        int c = 0;
-        for (int b = 1; b < i; b++)
+        Console.WriteLine($"You enter wrong Number '{str}' please renter vaild");
+        continue;
+    }
+
+    if (n == 0)
+    {
+        Console.WriteLine("You enter 0, thanks for using this program. Godd Bye!");
+        break;
+    }
+
+    if (n <= 1)
+    {
+        Console.WriteLine($"You entered an invalid integer '{n}' <= 1");
+        break;
+    }
+
+    int sum = 1;
+    Console.WriteLine("{0} and {1} divides {2}", 1, n, n);
+    for (int i = 2; i <= Math.Sqrt(n); i++)
+    {
+        if (n % i == 0)
         {
-            if (i % b == 0)
-            {
 
-                Console.WriteLine("The GCF{0} = {1}", j, b);
-                c = c + b;
-                j++;
-            }
-
-
+            Console.WriteLine("{0} and {1} divides {2}", i, n / i, n);
+            sum += i + (n/i);
         }
-        if (c == i)
-        {
-            Console.WriteLine("The number {0} is perfect number", i);
-        }
-        Console.WriteLine("Enter number bigger than 1 to start or 0 to exit");
-        //i = Convert.ToInt32(Console.ReadLine());
-        goto allam;
+    }
+    if (sum == n)
+    {
+        Console.WriteLine("The number {0} is perfect number", n);
     }
 }
-else {
-    Console.WriteLine("You enter wrong Number please renter vaild");
-    goto allam;
-        }
